ed921a28b8e4af9f0b6ee8dbc8a138604730718d
tanto <tantows001@gmail.com>
2024-01-10 11:05:02 +0700

    Nested Layouts

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import LineChart from "./line-chart"
+
+function BarChart(){
+    return <h1>Bar Chart</h1>
+}
+
+export default function Dashboard(){
+    return LineChart()
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound(){
+    return (
+        <div>
+            <h2>Page Not Founde</h2>
+            <p>Could not find requested resource</p>
+        </div>
+    )
+}
