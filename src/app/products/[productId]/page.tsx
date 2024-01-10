b29ee9fb255415dd38a1a4aa8cfc61c8c8bafff5
tanto <tantows001@gmail.com>
2024-01-10 20:28:02 +0700

    Title Metadata

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,3 +1,23 @@
+import { Metadata } from "next"
+import { resolve } from "path"
+
+export const generateMetadata = async ({params}:Props): Promise<Metadata> => {
+    const title = await new Promise(resolve=>{
+        setTimeout(()=>{
+            resolve(`iPhone ${params.productId}`)
+        }, 100)
+    })
+    return {
+        title: `Product ${title}`
+    }
+}
+
+type Props = {
+    params: {
+        productId: string
+    }
+}
+
 export default function ProductDetails( {params}: {
     params: {productId: string}
 } ){
